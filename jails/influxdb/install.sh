7e85470c407dfcecf756c525fc6f54e7b6e1330f
colemamd <mdcoleman001@gmail.com>
2020-04-15 20:35:00 -0400

    cleanup & minor bug fixes

--- jails/influxdb/install.sh
+++ jails/influxdb/install.sh
@@ -14,19 +14,26 @@ INCLUDES_PATH="${SCRIPT_DIR}/jails/influxdb/includes"
 DATABASE=${influxdb_database}
 
 # Enable influxdb
+iocage exec "${JAIL_NAME}" setenv INFLUX_CONFIG_PATH /usr/local/etc/influxdb.conf
 iocage exec "${JAIL_NAME}" sysrc influxd_enable="YES"
 
 # Copy and edit pre-written config files
 echo "Copying default config file"
-iocage exec ${JAIL_NAME} mkdir -p /usr/local/etc
-cp ${INCLUDES_PATH}/influxdb.conf /mnt/${GLOBAL_DATASET_IOCAGE}/jails/${JAIL_NAME}/root/usr/local/etc/
+iocage exec "${JAIL_NAME}" mkdir -p /usr/local/etc
+cp "${INCLUDES_PATH}"/influxdb.conf /mnt/"${global_dataset_iocage}"/jails/"${JAIL_NAME}"/root/usr/local/etc/
 
 # Start influxdb and wait for it to startup
 iocage exec "${JAIL_NAME}" service influxd start
 sleep 15
 
 # Create database and restart
-iocage exec "${JAIL_NAME}" "curl -i -XPOST http://localhost:8086/query --data-urlencode 'q=CREATE DATABASE ${DATABASE}'"
+if iocage exec "${JAIL_NAME}" curl -XPOST http://localhost:8086/query --data-urlencode "q=CREATE DATABASE ${DATABASE}"; then
+  echo "Database created."
+else 
+  echo "Database creation failed. Please attempt to create the database manually."
+  exit 1
+fi
+
 iocage exec "${JAIL_NAME}" service influxd restart
 	
 # Don't need /mnt/includes any more, so unmount it
