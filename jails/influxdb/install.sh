85e2778218176747ef712ce670d05f3cdc0c6954
colemamd <mdcoleman001@gmail.com>
2020-04-20 11:02:29 -0400

    fix persistence

--- jails/influxdb/install.sh
+++ jails/influxdb/install.sh
@@ -7,20 +7,18 @@
 #
 #####
 
-# Initialise defaults
+# Initialise variables
 JAIL_NAME="influxdb"
 JAIL_IP="$(sed 's|\(.*\)/.*|\1|' <<<"${influxdb_ip4_addr}" )"
 INCLUDES_PATH="${SCRIPT_DIR}/jails/influxdb/includes"
 DATABASE=${influxdb_database}
 
-# Enable influxdb
-iocage exec "${JAIL_NAME}" setenv INFLUX_CONFIG_PATH /usr/local/etc/influxdb.conf
+# Mount and configure proper configuration location
+cp -rf "${INCLUDES_PATH}/influxd.conf" "/mnt/${global_dataset_config}/${JAIL_NAME}/influxd.conf"
+iocage exec "${JAIL_NAME}" mkdir -p /config/db/data /config/db/meta /config/db/wal
+iocage exec "${JAIL_NAME}" chown -R influxd:influxd /config/db
+iocage exec "${JAIL_NAME}" sysrc influxd_conf="/config/influxd.conf"
 iocage exec "${JAIL_NAME}" sysrc influxd_enable="YES"
-
-# Copy and edit pre-written config files
-echo "Copying default config file"
-iocage exec "${JAIL_NAME}" mkdir -p /usr/local/etc
-cp "${INCLUDES_PATH}"/influxdb.conf /mnt/"${global_dataset_iocage}"/jails/"${JAIL_NAME}"/root/usr/local/etc/
 
 # Start influxdb and wait for it to startup
 iocage exec "${JAIL_NAME}" service influxd start
@@ -34,11 +32,6 @@ else
   exit 1
 fi
 
-iocage exec "${JAIL_NAME}" service influxd restart
-	
-# Don't need /mnt/includes any more, so unmount it
-iocage fstab -r "${JAIL_NAME}" "${INCLUDES_PATH}" /mnt/includes nullfs rw 0 0
-
 # Done!
 echo "Installation complete!"
 echo "You may connect InfluxDB plugins to the InfluxDB jail at http://${JAIL_IP}:8086."
@@ -46,8 +39,4 @@ echo ""
 echo "Database Information"
 echo "--------------------"
 echo "Database = ${DATABASE} at http://${JAIL_IP}:8086."
-echo ""
-echo "Configuration Information"
-echo "-------------------------"
-echo "The configuration file is located at /usr/local/etc/influxdb.conf"
 echo ""
