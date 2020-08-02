06e2baf5e21b71107e3323f4969f2cb5eb175d1f
colemamd <mdcoleman001@gmail.com>
2020-08-02 19:29:33 -0400

    up user auto created

--- blueprints/unifi/install.sh
+++ blueprints/unifi/install.sh
@@ -32,6 +32,7 @@ UP_USER="${!UP_USER:-$1}"
 
 # shellcheck disable=SC2154
 UP_PASS="jail_${1}_up_password"
+UP_PASS="${!UP_PASS}"
 INCLUDES_PATH="${SCRIPT_DIR}/blueprints/unifi/includes"
 
 if [ -z "${!DB_PASS}" ]; then
@@ -44,7 +45,7 @@ if [ -z "${!DB_JAIL}" ]; then
 	exit 1
 fi
 
-if [ -z "${!UP_PASS}" ]; then
+if [ -z "${UP_PASS}" ]; then
 	echo "up_password can't be empty"
 	exit 1
 fi
@@ -96,23 +97,36 @@ if [ "${!POLLER}" == true ]; then
   # shellcheck disable=SC2154
   cp "${INCLUDES_PATH}"/up.conf /mnt/"${global_dataset_config}"/"${1}"
   # shellcheck disable=SC2154
-  cp "${INCLUDES_PATH}"/rc/unifi_poller.rc /mnt/"${global_dataset_iocage}"/jails/"${1}"/root/usr/local/etc/rc.d/unifi_poller
-  chmod +x /mnt/"${global_dataset_iocage}"/jails/"${1}"/root/usr/local/etc/rc.d/unifi_poller
+  cp "${INCLUDES_PATH}"/rc/unifi-poller.rc /mnt/"${global_dataset_iocage}"/jails/"${1}"/root/usr/local/etc/rc.d/unifi-poller
+  chmod +x /mnt/"${global_dataset_iocage}"/jails/"${1}"/root/usr/local/etc/rc.d/unifi-poller
   iocage exec "${1}" sed -i '' "s|influxdbuser|${DB_USER}|" /config/up.conf
   iocage exec "${1}" sed -i '' "s|influxdbpass|${!DB_PASS}|" /config/up.conf
   iocage exec "${1}" sed -i '' "s|unifidb|${DB_NAME}|" /config/up.conf
   iocage exec "${1}" sed -i '' "s|unifiuser|${UP_USER}|" /config/up.conf
-  iocage exec "${1}" sed -i '' "s|unifipassword|${!UP_PASS}|" /config/up.conf
+  iocage exec "${1}" sed -i '' "s|unifipassword|${UP_PASS}|" /config/up.conf
   iocage exec "${1}" sed -i '' "s|dbip|http://${DB_IP}:8086|" /config/up.conf
 
+  #Create hash of UP passwd, then create the UP user
+  SALT=$(openssl rand -hex 4)
+  echo "Salt = ${SALT}"
+  UP_HASH=$(perl -e 'print crypt('"${UP_PASS}"', "\$6\$'${SALT}'");')
+  echo "Password = ${UP_PASS}"
+  echo "Hash = ${UP_HASH}"
+  iocage exec "${1}" mongo --port 27117 --quiet ace --eval 'db.admin.insert( { "name" : "'"${UP_USER}"'" , "x_shadow" : "'"${UP_HASH}"'" , "requires_new_password" : false } )' 
 
+  #Find default Admin ID and Site ID in Controller, use that to assign permissions to the UP user
+  UP_ADMIN_ID=$(iocage exec "${1}" mongo --port 27117 --quiet ace --eval 'db.admin.findOne( { "name" : "'"${UP_USER}"'" } )' | sed -ne 's/.*("//' -ne 's/").*//p')
+  echo "UP Admin ID = ${UP_ADMIN_ID}"
+  UP_SITE_ID=$(iocage exec "${1}" mongo --port 27117 --quiet ace --eval 'db.site.findOne( { "name" : "default" } , { "_id" : 1 } )' | sed -ne 's/.*("//' -ne 's/").*//p')
+  echo "UP Site ID = ${UP_SITE_ID}"
+  iocage exec "${1}" mongo --port 27117 --quiet ace --eval 'db.privilege.insert( { "admin_id" : "'"${UP_ADMIN_ID}"'" , "site_id" : "'"${UP_SITE_ID}"'" , "permissions" : [] , "role" : "readonly" } )'
+
+  iocage exec "${1}" service unifi restart
   iocage exec "${1}" sysrc unifi_poller_enable=YES
-  iocage exec "${1}" service unifi_poller start
+  iocage exec "${1}" service unifi-poller start
 
   echo "Installation complete!"
   echo "Unifi Controller is accessible at https://${JAIL_IP}:8443."
-  echo "Please login to the Unifi Controller and add ${UP_USER} as a read-only user."
-  echo "In Grafana, add Unifi-Poller as a data source."
 else
   echo "Installation complete!"
   echo "Unifi Controller is accessible at https://${JAIL_IP}:8443."
